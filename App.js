a3420fb89507dfb0b6f6a637b8e7643e536d090d
z3t4z00k <z3t4z00k@gmail.com>
2020-11-12 12:26:49 -0500

    Added Game Over screen

--- App.js
+++ App.js
@@ -1,28 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 
 import Header from "./components/Header/Header"
 
 import StartGameScreen from "./screens/StartGameScreen"
 import GameScreen from "./screens/GameScreen"
+import GameOverScreen  from "./screens/GameOver"
 
 import Colors from "./constants/Colors"
 
 export default function App() {
 
 	const [number, setNumber] = useState()
+	const [guesses, setGuesses] = useState()
 	const [gameState, setGameState] = useState(0)
+	const [screen, setScreen] = useState(<StartGameScreen onStart={onStartHandler} />)
 
 	const onStartHandler = input => {
 		setNumber(input)
 		setGameState(1)
 	}
 
+	const onGameStart = () => {
+		setNumber()
+		setGuesses()
+		setGameState(0)
+	}
+
+	const onGameFinish = (number, guesses) => {
+		setNumber(number)
+		setGuesses(guesses)
+		setGameState(2)
+	}
+
+	useEffect (()=>{
+		switch(gameState){
+			case 0: 
+				setScreen(<StartGameScreen onStart={onStartHandler} />)
+				break;
+			case 1:
+				setScreen(<GameScreen userNumber={number} onGameFinishHandler={onGameFinish}/>)
+				break;
+			case 2:
+				setScreen(<GameOverScreen number={number} guesses={guesses} onGameStartHandler={onGameStart}/>)
+				break;
+		}
+	}, [gameState])
+
 	return (
 		<View style={styles.screen}>
 			<Header text="Guess a Number" />
-			{gameState ? <GameScreen userNumber={number} /> : <StartGameScreen onStart={onStartHandler} />}
+			{screen}
 		</View>
 	);
 }
