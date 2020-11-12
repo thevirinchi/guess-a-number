6ba3bc9c5e3d7997fafd02d66ec8221441685104
z3t4z00k <z3t4z00k@gmail.com>
2020-11-12 09:57:44 -0500

    Shifted to StyleSheet object

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -41,20 +41,24 @@ const GameScreen = props => {
 	}
 
 	return (
-		<View style={{ flex: 1, flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
-			<View style={{ flexDirection: "column", alignItems: "center", justifyContent: "center", marginVertical: 16 }}>
+		<View style={styles.container}>
+			<View style={styles.guessContainer}>
 				<Body text="Opponent's Guess" />
-				<View style={{marginTop: 16,flexDirection: "column", justifyContent:"center", alignItems:"center",borderColor: Color.secondary, borderWidth: 2, borderRadius: 8, padding: 16}}><HeadingPrimary text={guess}/></View>
+				<View style={styles.guessBorder}><HeadingPrimary text={guess}/></View>
 			</View>
-			<View style={{ display: "flex", flexDirection: "row", width: "100%", alignItems: "center", justifyContent: "space-evenly", marginVertical: 16 }}>
-				<View style={{ width: "40%" }}><Button title="Lesser" onPress={lesserHandler} disabled={gameState} /></View>
-				<View style={{ width: "40%" }}><Button title="Greated" onPress={greatedHandler} disabled={gameState} /></View>
+			<View style={styles.buttonContainer}>
+				<View style={{ width: "40%" }}><Button title="Lesser" onPress={lesserHandler} disabled={gameState} color={Color.indicativeWarning}/></View>
+				<View style={{ width: "40%" }}><Button title="Greater" onPress={greatedHandler} disabled={gameState} color={Color.indicativeSuccess}/></View>
 			</View>
 		</View>
 	)
 }
 
 const styles = StyleSheet.create({
+	container: { flex: 1, flexDirection: "column", justifyContent: "center", alignItems: "center" },
+	guessContainer: { flexDirection: "column", alignItems: "center", justifyContent: "center", marginVertical: 16 },
+	guessBorder: {marginTop: 16,flexDirection: "column", justifyContent:"center", alignItems:"center",borderColor: Color.secondary, borderWidth: 4, borderRadius: 8, padding: 16, shadowOpacity: 0.24,shadowRadius: 6,shadowOffset: {width: 0, height: 2},shadowColor: Color.black, elevation: 4, backgroundColor:Color.whiteLight},
+	buttonContainer: { display: "flex", flexDirection: "row", width: "100%", alignItems: "center", justifyContent: "space-evenly", marginVertical: 16 },
 
 })
 
