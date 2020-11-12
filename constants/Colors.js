3b1b7647745845395813c1ae415039435dfa4953
z3t4z00k <z3t4z00k@gmail.com>
2020-11-12 07:52:02 -0500

    Created a colors object

---
+++ constants/Colors.js
@@ -0,0 +1,14 @@
+export default {
+	primaryLight: "#67daff",
+	primary: "#03a9f4",
+	primaryDark: "#007ac1",
+	secondaryLight: "#ffff81",
+	secondary: "#ffd54f",
+	secondaryDark: "#c8a415",
+	blackLight: "#484848",
+	black: "#212121",
+	blackDark: "#000000",
+	whiteLight: "#ffffff",
+	white: "#fafafa",
+	whiteDark: "#c7c7c7",
+}
