0c2c96ac876d2ded8e6ad72c52a5be71aeee832c
z3t4z00k <z3t4z00k@gmail.com>
2020-11-12 03:16:27 -0500

    Added card component

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -3,19 +3,20 @@ import {View, Text, StyleSheet, TextInput, Button} from 'react-native';
 
 import HeadingPrimary from "../components/Typo/Heading/HeadingPrimary"
 import Body from "../components/Typo/Body/Body"
+import Card from "../components/Card/Card"
 
 const StartGameScreen = props => {
 	return (
 		<View style={styles.screen}>
 			<HeadingPrimary text="Start a New Game!"/>
-			<View style={styles.numberInput_container}>
+			<Card>
 				<Body text="Enter a number"/>
 				<TextInput placeholder="1-100" style={styles.numberInput}/>
 				<View style={styles.numberInput_buttonContainer}>
 					<View style={styles.numberInput_button}><Button title="Set"></Button></View>
 					<View style={styles.numberInput_button}><Button title="Cancel"></Button></View>
 				</View>
-			</View>
+			</Card>
 		</View>
 	)
 }
@@ -35,22 +36,6 @@ const styles = StyleSheet.create({
 		minHeight: 40,
 		textAlign: "center",
 		borderRadius: 24
-	},
-	numberInput_container: {
-		display: "flex",
-		flexDirection: "column",
-		alignItems: "center",
-		justifyContent: "center",
-		width: "100%",
-		padding: 16,
-		shadowOpacity: 0.24,
-		shadowRadius: 6,
-		shadowOffset: {width: 0, height: 2},
-		shadowColor: "#212121",
-		elevation: 8,
-		backgroundColor: "#fff",
-		borderRadius: 8,
-		marginVertical: 16
 	},
 	numberInput_buttonContainer: {
 		display: "flex",
