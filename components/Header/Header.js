1f3d2cd82ebc2eed4b1330ffd09ba6101db40b7a
z3t4z00k <z3t4z00k@gmail.com>
2020-11-12 03:05:22 -0500

    Improved header style

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -13,18 +13,25 @@ const Header = props => {
 const styles = StyleSheet.create({
 	header: {
 		width: "100%",
-		padding: 32,
-		paddingTop: 40,
+		paddingBottom: 32,
+		paddingTop: 48,
+		paddingHorizontal: 24,
 		height: 80,
 		display: "flex",
 		flexDirection: "row",
 		alignItems: "center",
 		justifyContent: "flex-start",
-		backgroundColor: "#ff7043"
+		backgroundColor: "#ff7043",
+		shadowOpacity: 0.25,
+		shadowRadius: 6,
+		shadowOffset: {width: 0, height: 2},
+		shadowColor: "#212121",
+		elevation: 8
 	},
 	header_title: {
 		fontSize: 18,
-		color: "#212121"
+		color: "#212121",
+		fontWeight: "600"
 	}
 })
 
