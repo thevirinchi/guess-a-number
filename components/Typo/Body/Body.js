0d74ef5a430aafffe73485438ae4b7875db390cb
z3t4z00k <z3t4z00k@gmail.com>
2020-11-12 03:04:01 -0500

    Created a component for normal text

---
+++ components/Typo/Body/Body.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {View, Text, StyleSheet, TextInput} from 'react-native'
+
+const Body = props => {
+	return (
+		<View style={styles.container}>
+			<Text style={styles.body}>{props.text}</Text>
+		</View>
+	)
+}
+
+const styles = StyleSheet.create({
+	container: {
+		paddingVertical: 4,
+		display: "flex",
+		flexDirection: "column",
+		alignItems: "center"
+	},
+	body: {
+		fontSize: 16
+	}
+})
+
+export default Body;
