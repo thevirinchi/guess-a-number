18b1ea63770d402b84c2e35b43ccbefe22d88e91
z3t4z00k <z3t4z00k@gmail.com>
2020-11-12 09:16:23 -0500

    Linked multiple screens

--- App.js
+++ App.js
@@ -1,18 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 
 import Header from "./components/Header/Header"
 
 import StartGameScreen from "./screens/StartGameScreen"
+import GameScreen from "./screens/GameScreen"
 
 import Colors from "./constants/Colors"
 
 export default function App() {
+
+	const [number, setNumber] = useState()
+	const [gameState, setGameState] = useState(0)
+
+	const onStartHandler = input => {
+		setNumber(input)
+		setGameState(1)
+	}
+
 	return (
 		<View style={styles.screen}>
-			<Header text="Guess a Number"/>
-			<StartGameScreen/>
+			<Header text="Guess a Number" />
+			{gameState ? <GameScreen userNumber={number} /> : <StartGameScreen onStart={onStartHandler} />}
 		</View>
 	);
 }
