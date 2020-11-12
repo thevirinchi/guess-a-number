bdfcb38f010cec373f543fc6be823d73eff54934
z3t4z00k <z3t4z00k@gmail.com>
2020-11-12 09:27:36 -0500

    Fixed formating

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,21 +1,17 @@
 import React, { useEffect, useState } from 'react'
-import {View, Text, StyleSheet, RecyclerViewBackedScrollView, Button, Alert} from 'react-native'
+import { View, Text, StyleSheet, RecyclerViewBackedScrollView, Button, Alert } from 'react-native'
 
 import Body from "../components/Typo/Body/Body"
 
 const GameScreen = props => {
 
-	useEffect(()=>{
-		console.log(props.userNumber)
-	}, [])
-
 	const generateRandomNum = (min, max, exclude) => {
 		min = Math.ceil(min)
 		max = Math.floor(max)
-		const randomNumber = Math.floor(Math.random() * (max-min) + min)
-		if(randomNumber === props.userNumber)
+		const randomNumber = Math.floor(Math.random() * (max - min) + min)
+		if (randomNumber === props.userNumber)
 			setGameState(true)
-		return (randomNumber === exclude)? generateRandomNum(min, max, exclude):randomNumber
+		return (randomNumber === exclude) ? generateRandomNum(min, max, exclude) : randomNumber
 	}
 
 	const [minGuess, setMinGuess] = useState(0)
@@ -23,24 +19,20 @@ const GameScreen = props => {
 	const [gameState, setGameState] = useState(false)
 	const [guess, setGuess] = useState(generateRandomNum(minGuess, maxGuess, props.userNumber))
 
-	useEffect(()=>{
-		console.log(guess)
-	}, [guess])
-
-	useEffect(()=>{
+	useEffect(() => {
 		setGuess(generateRandomNum(minGuess, maxGuess, guess))
 	}, [minGuess, maxGuess])
 
 	const lesserHandler = () => {
-		if(guess<props.userNumber)
-			Alert.alert("Don\'t Lie!", "You know that\'s wrong...", [{text: "Sorry", style: "cancel"}])
+		if (guess < props.userNumber)
+			Alert.alert("Don\'t Lie!", "You know that\'s wrong...", [{ text: "Sorry", style: "cancel" }])
 		else
 			setMaxGuess(guess)
 	}
 
 	const greatedHandler = () => {
-		if(guess>props.userNumber)
-			Alert.alert("Don\'t Lie!", "You know that\'s wrong...", [{text: "Sorry", style: "cancel"}])
+		if (guess > props.userNumber)
+			Alert.alert("Don\'t Lie!", "You know that\'s wrong...", [{ text: "Sorry", style: "cancel" }])
 		else
 			setMinGuess(guess)
 	}
@@ -48,12 +40,12 @@ const GameScreen = props => {
 	return (
 		<View>
 			<View>
-				<Body text="Opponent's Guess"/>
+				<Body text="Opponent's Guess" />
 				<Text>{guess}</Text>
 			</View>
 			<View>
-				<View><Button title="Lesser" onPress={lesserHandler} disabled={gameState}/></View>
-				<View><Button title="Greated" onPress={greatedHandler} disabled={gameState}/></View>
+				<View><Button title="Lesser" onPress={lesserHandler} disabled={gameState} /></View>
+				<View><Button title="Greated" onPress={greatedHandler} disabled={gameState} /></View>
 			</View>
 		</View>
 	)
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import {View, Text, StyleSheet, TextInput, Button, TouchableWithoutFeedback, Keyboard, Alert} from 'react-native';
+import { View, StyleSheet, TextInput, Button, TouchableWithoutFeedback, Keyboard, Alert } from 'react-native';
 
 import HeadingPrimary from "../components/Typo/Heading/HeadingPrimary"
 import Body from "../components/Typo/Body/Body"
@@ -16,22 +16,22 @@ const StartGameScreen = props => {
 	}
 
 	const setNumberHandler = () => {
-		if(isNaN(number) || number < 0 || number > 99)
-			Alert.alert("Invalid number!", "Please enter a value in the range 0-99 only.", [{text: "Okay", style: "destructive", onPress: setNumber()}])
+		if (isNaN(number) || number < 0 || number > 99)
+			Alert.alert("Invalid number!", "Please enter a value in the range 0-99 only.", [{ text: "Okay", style: "destructive", onPress: setNumber() }])
 		else
 			props.onStart(number)
 	}
 
 	return (
-		<TouchableWithoutFeedback onPress={()=>{Keyboard.dismiss}}>
+		<TouchableWithoutFeedback onPress={() => { Keyboard.dismiss }}>
 			<View style={styles.screen}>
-				<HeadingPrimary text="Start a New Game!"/>
+				<HeadingPrimary text="Start a New Game!" />
 				<Card>
-					<Body text="Enter a number"/>
-					<TextInput placeholder="0-99" style={styles.numberInput} autoCorrect={false} keyboardType="number-pad" maxLength={2} onChangeText={numberChangeHandler} value={number}/>
+					<Body text="Enter a number" />
+					<TextInput placeholder="0-99" style={styles.numberInput} autoCorrect={false} keyboardType="number-pad" maxLength={2} onChangeText={numberChangeHandler} value={number} />
 					<View style={styles.numberInput_buttonContainer}>
 						<View style={styles.numberInput_button}><Button title="Set" color={Colors.indicativeSuccess} onPress={setNumberHandler}></Button></View>
-						<View style={styles.numberInput_button}><Button title="Cancel" color={Colors.indicativeWarning} onPress={()=>{setNumber()}}></Button></View>
+						<View style={styles.numberInput_button}><Button title="Cancel" color={Colors.indicativeWarning} onPress={() => { setNumber() }}></Button></View>
 					</View>
 				</Card>
 			</View>
