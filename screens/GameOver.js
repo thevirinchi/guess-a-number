db6c30dec55af7b87dffd64482494a77e833e18f
z3t4z00k <z3t4z00k@gmail.com>
2020-11-13 01:11:05 -0500

    Added background image

--- screens/GameOver.js
+++ screens/GameOver.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {View, Button, StyleSheet} from 'react-native'
+import {View, Button, StyleSheet, ImageBackground} from 'react-native'
 
 import HeadingPrimary from "../components/Typo/Heading/HeadingPrimary"
 import HeadingSecondary from "../components/Typo/Heading/HeadingSecondary"
@@ -12,17 +12,20 @@ import Colors from "../constants/Colors"
 const GameOver = props => {
 	return (
 		<View style={styles.container}>
-			<HeadingSecondary text="Game Over!"/>
-			<HeadingPrimary text={"The number was " + props.number}/>
-			<Body text={"Number of Guesses: " + props.guesses}/>
-			<View style={{width: "50%", marginTop: 16}}><Button title="Restart" onPress={props.onGameStartHandler}/></View>
-			<ConfettiCannon explosionSpeed={500} count={200} origin={{x: -10, y: 0}} fadeOut={true}/>
+			<ImageBackground source={require('../assets/images/success.png')} style={styles.background}>
+				<HeadingSecondary text="Game Over!"/>
+				<HeadingPrimary text={"The number was " + props.number}/>
+				<Body text={"Number of Guesses: " + props.guesses}/>
+				<View style={{width: "50%", marginTop: 16}}><Button title="Restart" onPress={props.onGameStartHandler}/></View>
+				<ConfettiCannon explosionSpeed={500} count={200} origin={{x: -10, y: 0}} fadeOut={true}/>
+			</ImageBackground>
 		</View>
 	)
 }
 
 const styles = StyleSheet.create({
-	container: {flex: 1, flexDirection: "column", justifyContent:"center", alignItems:"center"}
+	container: {flex: 1, flexDirection: "column", justifyContent:"center", alignItems:"center", width: "100%"},
+	background: {flex: 1, resizeMode: "cover", justifyContent: "center", alignItems: "center", width: "100%"}
 })
 
 export default GameOver;
